3af2b396d119eec980313ddc645715b5250f73cf
Mariami985 <73032684+Mariami985@users.noreply.github.com>
2021-04-25 20:51:07 +0400

    create app.js

---
+++ app.js
@@ -0,0 +1,86 @@
+//დავალება 1
+
+// let arr = [
+// 'javascript', 
+// 'php', 
+// 'c#', 
+// 'python',
+//  'c++',
+// 'kotlin', 
+//  'c', 
+//  'swift'
+// ];
+
+// for (let i = 0; i < arr.length; i++){
+
+//     if (arr[i].length < 5){
+//         continue;
+//     }
+//     console.log(arr[i]);
+// };
+
+// დავალება 2
+// let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0){
+//         console.log(arr[i]);
+//     }
+// };
+
+//  დავალება 3
+
+//  let arr = [
+//  [2, -3, 5, 11],
+//  [1, -30, -11, 100],
+//  [-1, -3, -4]
+//  ];
+//  for (let i = 0; i < arr.length; i++) {
+//  let arr2 = arr[i];
+//  for (let x = 0; x < arr2.length; x++){
+//  if (arr2[x] > 0){
+//   console.log(arr2[x]);
+//    }
+//        }
+//   };
+
+  //  დავალება 4
+ 
+  //  let numbers = [1, 15, 6, 30];
+  // function getMax(numbers){
+  //  let maxValue = 0;
+  //  for (let i = 0; i < numbers.length; i ++) {
+  //      let x = numbers[i];
+  //      if ( x > maxValue) {
+  //       maxValue = x;
+  //      }
+  //  }
+  //  return maxValue
+  // }
+
+  //  console.log(getMax([1,15,6,30]));
+
+// დავალება 5
+
+
+// let user = {
+//   firstname: 'giorgi',
+//   lastname: 'saakadze',
+//   age: 32,
+//   isLoggedIn: true
+// }
+
+// function printName (user) {
+//   if (user.isLoggedIn){
+//     return user.firstname + ' ' + user.lastname;
+//   }
+//   return false;
+  
+// }
+// console.log(printName(user));
+
+
+
+
+
+
